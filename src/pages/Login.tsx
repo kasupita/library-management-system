26dd9ad9754f4085a7c96bc858c67c7161f244ff
kasupita <pitawalalochana@gmail.com>
2026-07-06 11:20:12 +0530

    Remove unused state variable 'identifier' from Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -15,7 +15,7 @@ export default function Login() {
   const navigate = useNavigate();
   
 
-  const [identifier, setIdentifier] = useState('');
+ 
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
